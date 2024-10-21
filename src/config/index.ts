288046216be4a77cc171ed2cedad1c2e0a8c5b76
RezkiBawono <rezkibawono@gmail.com>
2024-10-21 13:20:01 +0700

    changing title

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,9 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
-  description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+  title: "Rezki Test Website",
+  author: "Rezki",
+  description: "Test Description",
   lang: "en",
   siteLogo: "/alejandro-small.jpg",
   navLinks: [
